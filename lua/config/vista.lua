4b83560d2624d7e54eaadac6f001545485d9a0f5
PandeCode <pandeshawnbenjamin@gmail.com>
2021-08-15 02:05:27 +0300

    vista

--- lua/config/vista.lua
+++ lua/config/vista.lua
@@ -1,15 +1,6 @@
 vim.cmd "source $HOME/.config/nvim/vimscript/config/vista.vim"
 
+vim.g.vista_log_file = "/home/shawn/vista.log"
 vim.g.vista_icon_indent = {"╰─▸ ", "├─▸ "}
 vim.g.vista_fzf_preview = {"right:50%"}
-vim.g.vista_default_executive = "coc"
-
-vim.g.vista = {
-    renderer = {
-        enable_icon = 1,
-        icons = {
-            variable = "",
-            ["function"] = ""
-        }
-    }
-}
+vim.g.vista_default_executive = "nvim_lsp"

b0a2126f37cf6de3aa3d48ee180c6e66abb5570e
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-04 13:20:45 +0300

    luaformat && install

--- install.sh
+++ install.sh
@@ -5,3 +5,15 @@ python3 -m pip install pynvim cmakelang cmake-language-server black-but-with-tab
 python2 -m pip install pynvim
 
 yarn global add neovim pyright stylelint-lsp svelte-language-server typescript-language-server bash-language-server vim-language-server yaml-language-server vscode-langservers-extracted
+
+ln -s ./dotfiles/.ccls             ~/.ccls
+ln -s ./dotfiles/.clang-format     ~/.clang-format
+ln -s ./dotfiles/.cmake-format     ~/.cmake-format
+ln -s ./dotfiles/.ctags            ~/.ctags
+ln -s ./dotfiles/.eslintrc         ~/.eslintrc
+ln -s ./dotfiles/.lua-format       ~/.lua-format
+ln -s ./dotfiles/.prettierrc       ~/.prettierrc
+ln -s ./dotfiles/.pylintrc         ~/.pylintrc
+ln -s ./dotfiles/.rgignore         ~/.rgignore
+ln -s ./dotfiles/.vimspector.json  ~/.vimspector.json
+

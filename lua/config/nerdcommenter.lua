5696553ed5f608942d4f13fadef09130faea595e
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-05 03:18:31 +0300

    nerdcommenter

--- lua/config/nerdcommenter.lua
+++ lua/config/nerdcommenter.lua
@@ -1,1 +1,3 @@
-vim.cmd "source $HOME/.config/nvim/vimscript/config/nerdcommmenter.vim"
+vim.api.nvim_set_keymap("i", "<c-_>", "<ESC>:call NERDComment('n', 'toggle')<CR>i", {noremap=true})
+vim.api.nvim_set_keymap("n", "<c-_>", ":call NERDComment('', 'toggle')<CR>",        {noremap=true})
+vim.api.nvim_set_keymap("v", "<c-_>", ":call NERDComment('x', 'toggle')<CR>v",      {noremap=true})
--- vimscript/config/nerdcommmenter.vim
+++
@@ -1,3 +0,0 @@
-imap <c-_> <ESC>:call NERDComment('n', 'toggle')<CR>i
-nmap <c-_> :call NERDComment('n', 'toggle')<CR>
-vmap <c-_> :call NERDComment('x', 'toggle')<CR>v

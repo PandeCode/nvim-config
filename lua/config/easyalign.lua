f0d0a3e04f67ff35b6b247c8b3f490f270eb1598
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-05 18:51:24 +0300

    binds

--- lua/config/easyalign.lua
+++ lua/config/easyalign.lua
@@ -1,8 +1,6 @@
-vim.api.nvim_set_keymap(n,   "ga", "<Plug>(LiveEasyAlign)", {})
+vim.api.nvim_set_keymap("n", "ga", "<Plug>(LiveEasyAlign)", {})
 vim.api.nvim_set_keymap("x", "ga", "<Plug>(LiveEasyAlign)", {})
 
-vim.g.easy_align_delimiters["d"] = {
-	pattern = " \\ze\\S\\+\\s*[;=]",
-	left_margin = 0,
-	right_margin = 0
+vim.g.easy_align_delimiters = {
+	d = {pattern = " \\ze\\S\\+\\s*[;=]", left_margin = 0, right_margin = 0}
 }

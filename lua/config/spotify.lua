5bd7b1268135fca5a91ff2afb8cffc0d16bf3b2d
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-05 04:09:56 +0300

    spofity

--- lua/config/spotify.lua
+++ lua/config/spotify.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_keymap("n", "<space>sn", ":AsyncRun spotify-cli next<cr>", { noremap=true, slient=true })
-vim.api.nvim_set_keymap("n", "<space>sp", ":AsyncRun spotify-cli prev<cr>", { noremap=true, slient=true })
-vim.api.nvim_set_keymap("n", "<space>s<space>", ":AsyncRun spotify-cli toggle<cr>", { noremap=true, slient=true })
+vim.api.nvim_set_keymap("n", "<space>sn", ":AsyncRun spotify-cli next<cr>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("n", "<space>sp", ":AsyncRun spotify-cli prev<cr>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("n", "<space>s<space>", ":AsyncRun spotify-cli toggle<cr>", { noremap=true, silent=true })

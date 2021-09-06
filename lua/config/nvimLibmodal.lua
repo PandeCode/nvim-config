bdcb73a412e7486a4359adedf7f77f6834786632
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-06 08:55:42 +0300

    resize and move modes

--- lua/config/nvimLibmodal.lua
+++ lua/config/nvimLibmodal.lua
@@ -1,12 +1,12 @@
-WinResizeMode   = {
-	["j"]       = "echom \"Hello down\"",
-	["k"]       = "echom \"Hello up\"",
-	["h"]       = "echom \"Hello left\"",
-	["l"]       = "echom \"Hello right\"",
-	["<down>"]  = "echom \"Hello down\"",
-	["<up>"]    = "echom \"Hello up\"",
-	["<left>"]  = "echom \"Hello left\"",
-	["<right>"] = "echom \"Hello right\""
+local WinResizeMode   = {
+	["j"]       = "wincmd -",
+	["<down>"]  = "wincmd -",
+	["k"]       = "wincmd +",
+	["<up>"]    = "wincmd +",
+	["h"]       = "wincmd <",
+	["<left>"]  = "wincmd <",
+	["l"]       = "wincmd >",
+	["<right>"] = "wincmd >"
 }
 
 function CallWinResizeMode()
@@ -17,4 +17,21 @@ vim.api.nvim_set_keymap(
 	"n", "<leader>wr", ":lua CallWinResizeMode()<cr>", {noremap = true}
 )
 
-CallWinResizeMode()
+local WinMoveMode   = {
+	["j"]       = "wincmd J",
+	["<down>"]  = "wincmd J",
+	["k"]       = "wincmd K",
+	["<up>"]    = "wincmd K",
+	["h"]       = "wincmd H",
+	["<left>"]  = "wincmd H",
+	["l"]       = "wincmd L",
+	["<right>"] = "wincmd L"
+}
+
+function CallWinMoveMode()
+	require("libmodal").mode.enter("WinMoveMode", WinMoveMode)
+end
+
+vim.api.nvim_set_keymap(
+	"n", "<leader>wm", ":lua CallWinMoveMode()<cr>", {noremap = true}
+)

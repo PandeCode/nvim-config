cca40390d39228116d2f191c65c7e28697604f76
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-10 11:36:52 +0300

    neoformat js

--- lua/config/neoformat.lua
+++ lua/config/neoformat.lua
@@ -13,11 +13,19 @@ vim.g.neoformat_lua_luaformat = {
 	stdin = 1
 }
 
-vim.g.neoformat_enabled_lua = {"luaformat"}
-vim.g.neoformat_enabled_json = {"prettier"}
-vim.g.neoformat_enabled_python = {"black"}
-vim.g.neoformat_cmake_cmakeformat = {args = {}, stdin = 1, exe = "cmake-format"}
-vim.g.neoformat_only_msg_on_error = 0
+vim.g.neoformat_enabled_lua             = {"luaformat"}
+vim.g.neoformat_enabled_json            = {"prettier"}
+vim.g.neoformat_enabled_js              = {"prettier"}
+vim.g.neoformat_enabled_ts              = {"prettier"}
+vim.g.neoformat_enabled_jsx             = {"prettier"}
+vim.g.neoformat_enabled_tsx             = {"prettier"}
+vim.g.neoformat_enabled_typescript      = {"prettier"}
+vim.g.neoformat_enabled_javascript      = {"prettier"}
+vim.g.neoformat_enabled_typescriptreact = {"prettier"}
+vim.g.neoformat_enabled_javascriptreact = {"prettier"}
+vim.g.neoformat_enabled_python          = {"black"}
+vim.g.neoformat_cmake_cmakeformat       = {args = {}, stdin = 1, exe = "cmake-format"}
+vim.g.neoformat_only_msg_on_error       = 0
 
 vim.api.nvim_set_keymap("n", "<Space>ff", ":Neoformat<CR>", {noremap = true})
 vim.api.nvim_set_keymap("v", "<Space>ff", ":Neoformat<CR>", {noremap = true})

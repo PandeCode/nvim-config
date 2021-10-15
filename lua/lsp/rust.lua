1036fe70de29fbc5ade296432800b7b1f82e3676
PandeCode <pandeshawnbenjamin@gmail.com>
2021-10-15 11:37:50 +0300

    try virtual text rust

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -3,12 +3,12 @@ NVIM_LSP.rust_analyzer.setup({
 	capabilities = CAPABILITIES,
 	flags = FLAGS,
 	--
-	--handlers = {
-	--["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	---- Disable virtual_text
-	--virtual_text = true,
-	--}),
-	--},
+	handlers = {
+	["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	-- Disable virtual_text
+	virtual_text = true,
+	}),
+	},
 
 	filetypes = { "rust" },
 })

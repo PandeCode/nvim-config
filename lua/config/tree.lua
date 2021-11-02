5a2c84ae5ca9e4191c34c0c69183e6fd5c5a99e6
PandeCode <pandeshawnbenjamin@gmail.com>
2021-11-02 19:39:20 +0300

    rm hello.py #idk && nvim-tree settings update

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -3,6 +3,9 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
 -- following options are the default
 require("nvim-tree").setup({
+
+	hide_dotfiles = 1, --0 by default, this option hides files and folders starting with a dot `.`
+	ignore = { ".git", "node_modules", ".cache" }, --empty by default
 	-- disables netrw completely
 	disable_netrw = true,
 	-- hijack netrw window on startup
@@ -99,12 +102,10 @@ require("nvim-tree").setup({
 
 vim.g.nvim_tree_side = "right" --left by default
 vim.g.nvim_tree_width = 40 --30 by default, can be width_in_columns or 'width_in_percent%'
-vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache" } --empty by default
 vim.g.nvim_tree_gitignore = 1 --0 by default
 vim.g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" } --empty by default, don't auto open tree on specific filetypes.
 vim.g.nvim_tree_quit_on_open = 1 --0 by default, closes the tree when you open a file
 vim.g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = "=~" --This is the default. See =help filename-modifiers for more options

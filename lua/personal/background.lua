1f7ecf48dbdebaf5e0297bfb1e2b28cdbb0f32e5
PandeCode <pandeshawnbenjamin@gmail.com>
2021-11-13 17:40:18 +0300

    script referrence

--- lua/personal/background.lua
+++ lua/personal/background.lua
@@ -1,3 +1,6 @@
+-- https://github.com/PandeCode/dotfiles/blob/main/PERSONAL_PATH/randbg
+-- https://github.com/PandeCode/dotfiles/blob/main/PERSONAL_PATH/prevbg
+
 vim.api.nvim_set_keymap(Keys.N, "<SPACE>bp", "<CMD>:AsyncRun prevbg<CR>", Keys.NoremapSilent)
 vim.api.nvim_set_keymap(Keys.N, "<SPACE>br", "<CMD>:AsyncRun randbg<CR>", Keys.NoremapSilent)
 vim.api.nvim_set_keymap(Keys.N, "<SPACE>bn", "<CMD>:AsyncRun randbg<CR>", Keys.NoremapSilent)

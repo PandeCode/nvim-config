d66fb874e1ca3a8191e6e60208ce80ccd2b632b7
PandeCode <pandeshawnbenjamin@gmail.com>
2021-11-18 16:00:35 +0300

    treesitter org

--- lua/config/orgmode.lua
+++ lua/config/orgmode.lua
@@ -1,4 +1,14 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.org = {
+	install_info = {
+		url = "https://github.com/milisims/tree-sitter-org",
+		revision = "main",
+		files = { "src/parser.c", "src/scanner.cc" },
+	},
+	filetype = "org",
+}
+
 require("orgmode").setup({
-  org_agenda_files = {'~/dev/personal/org/*'},
-  org_default_notes_file = '~/dev/personal/refile.org',
+	org_agenda_files = { "~/dev/personal/org/*" },
+	org_default_notes_file = "~/dev/personal/refile.org",
 })

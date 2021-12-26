9816a34a66cc358e72f9e3796da94065bacf6039
PandeCode <pandeshawnbenjamin@gmail.com>
2021-12-26 19:58:42 +0300

    rust snippets | better lua support for awesome and not affecting nvim but also providing 5.4 tooling for other projects

--- lua/lsp/sumneko_lua.lua
+++ lua/lsp/sumneko_lua.lua
@@ -1,8 +1,35 @@
-local runtime_path = vim.split(package.path, ";")
+-- local runtime_path = vim.split(package.path, ";")
+-- local runtime_path = {}
+
+lua_type = "normal"
+
+local cwd = vim.fn.getcwd()
+
+local globals = {}
+local libraries = {}
+local runtime_path = {}
+local version = "Lua 5.4"
+
+if cwd == "/home/shawn/.config/nvim" or cwd == "/home/shawn/dotfiles/config/nvim" then
+	lua_type = "nvim"
+
+	globals = { "vim", "use", "use_rocks" }
+	libraries = vim.api.nvim_get_runtime_file("", true)
+	runtime_path = vim.split(package.path, ";")
+	version = "LuaJIT"
+elseif cwd == "/home/shawn/.config/awesome" or cwd == "/home/shawn/dotfiles/config/awesome" then
+	lua_type = "awesome"
+
+	globals = { "awesome", "tag", "screen", "client", "taglist_buttons", "mouse" }
+	libraries = { "/usr/share/awesome/lib" }
+	runtime_path = { "/usr/share/awesome/lib/?.lua", "/usr/share/awesome/lib/?/init.lua" }
+	version = "Lua 5.3"
+end
+
+table.insert(runtime_path, "?.lua")
+table.insert(runtime_path, "?/init.lua")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-
---cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
 
 NVIM_LSP.sumneko_lua.setup({
 	on_attach = ON_ATTACH,
@@ -13,23 +40,16 @@ NVIM_LSP.sumneko_lua.setup({
 	settings = {
 		Lua = {
 			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
+				version = version,
 				path = runtime_path,
 			},
 			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim", "use", "use_rocks" },
+				globals = globals,
+				disable = { "lowercase-global" },
 			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
+			workspace = { library = libraries }, -- Make the server aware of  runtime files
+			-- send telemetry data containing a randomized but unique identifier
+			telemetry = { enable = true },
 		},
 	},
 })

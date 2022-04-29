ae9eed43a6bc293f8955a6fcd764b9c73b1b6859
PandeCode <pandeshawnbenjamin@gmail.com>
2022-04-29 21:42:24 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-sudo yay -S clang cmake ccls ripgrep ttf-fira-code ctags ueberzug ffmpegthumbnailer poppler fontpreview fmui-git shfmt glsl-language-server
+paru -S clang cmake ccls ripgrep ttf-fira-code ctags ueberzug ffmpegthumbnailer poppler fontpreview fmui-git shfmt glsl-language-server
 
 python3 -m pip install pynvim cmakelang cmake-language-server black-but-with-tabs-instead-of-spaces
 python2 -m pip install pynvim

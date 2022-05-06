127e781f017ada4b19435c9a683610c99345ff54
PandeCode <pandeshawnbenjamin@gmail.com>
2022-05-06 21:23:04 +0300

    added glsl to copilot

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -11,6 +11,7 @@ vim.g.copilot_filetypes = {
 	bash                = true,
 	cpp                 = true,
 	css                 = true,
+	glsl                = true,
 	html                = true,
 	javascript          = true,
 	javascriptreact     = true,

b8e7f06e49b08844ad076f0ed8b859554fd31bd8
PandeCode <pandeshawnbenjamin@gmail.com>
2022-12-24 22:33:59 +0300

    lsp

--- lua/lsp/emmet.lua
+++ lua/lsp/emmet.lua
@@ -1,30 +1,25 @@
-local configs = require("lspconfig/configs")
+NVIM_LSP.emmet_ls.setup({
+	capabilities = CAPABILITIES,
+	flags = FLAGS,
 
-if not NVIM_LSP.emmet_ls then
-	configs.emmet_ls = {
-		default_config = {
-			cmd = { "emmet-ls", "--stdio" },
-			filetypes = {
-				"html",
-				"css",
-				"jsx",
-				"tsx",
-				"javascriptreact",
-				"typescriptreact",
-				"text",
-				"txt",
-				"rust",
-			},
-			root_dir = function(fname)
-				return vim.loop.cwd()
-			end,
-			settings = {
-				includeLanguages = {
-					rust = "html",
-				}
-			},
+	cmd = { "emmet-ls", "--stdio" },
+	filetypes = {
+		"html",
+		"css",
+		"jsx",
+		"tsx",
+		"javascriptreact",
+		"typescriptreact",
+		"text",
+		"txt",
+		"rust",
+	},
+	root_dir = function(fname)
+		return vim.loop.cwd()
+	end,
+	settings = {
+		includeLanguages = {
+			rust = "html",
 		},
-	}
-end
-
-NVIM_LSP.emmet_ls.setup({ capabilities = CAPABILITIES, flags = FLAGS })
+	},
+})
--- lua/lsp/glsl.lua
+++ lua/lsp/glsl.lua
@@ -1,19 +1,11 @@
-local configs = require("lspconfig/configs")
+NVIM_LSP.glslls.setup({
+	on_attach = ON_ATTACH,
+	capabilities = CAPABILITIES,
+	flags = FLAGS,
 
-if not NVIM_LSP.glsl then
-	configs.glsl_ls = {
-		default_config = {
-			cmd = { "glslls", "--stdin" },
-			filetypes = { "glsl", "hlsl", "vert", "frag" },
-			---@diagnostic disable-next-line: unused-local
-			root_dir = function(fname)
-				return vim.loop.cwd()
-			end,
-
-    		single_file_support = true,
-			settings = {},
-		},
-	}
-end
-
-NVIM_LSP.glsl.setup({ on_attach = ON_ATTACH, capabilities = CAPABILITIES, flags = FLAGS })
+	-- cmd = { "glslls", "--stdin" },
+	filetypes = { "glsl", "hlsl", "vert", "frag" },
+	-- root_dir = function(fname) return vim.loop.cwd() end,
+	-- single_file_support = true,
+	-- settings = {},
+})

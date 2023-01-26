1ed3db37d5495123108e6a64c4baefe3ddef47dc
PandeCode <pandeshawnbenjamin@gmail.com>
2023-01-27 02:04:09 +0300

    fmt

--- lua/plugins/neoformat_conf.lua
+++ lua/plugins/neoformat_conf.lua
@@ -8,34 +8,34 @@ vim.g.neoformat_basic_format_retab = 0
 vim.g.neoformat_basic_format_trim = 1
 
 vim.g.neoformat_sh_shfmt = {
-        exe = "shfmt",
-        stdin = 1,
-        args = {
-                "-sr", -- Redirect operators will be followed by a space.
-                "-ci", -- Switch cases will be indented.
-                "-s", -- Simplify the code.
-                "-i",  -- Indent: 0 for tabs (default), >0 for number of spaces.
-                "0",
-        },
+	exe = "shfmt",
+	stdin = 1,
+	args = {
+		"-sr", -- Redirect operators will be followed by a space.
+		"-ci", -- Switch cases will be indented.
+		"-s", -- Simplify the code.
+		"-i", -- Indent: 0 for tabs (default), >0 for number of spaces.
+		"0",
+	},
 }
 
 vim.g.neoformat_stylua = {
-        exe = "stylua",
-        stdin = 1,
-        args = {
-                "--config-file=~/.stylua.toml",
-                "--stdin-filepath",
-                '"%=p"',
-                "--",
-                "-",
-        },
+	exe = "stylua",
+	stdin = 1,
+	args = {
+		"--config-file=~/.stylua.toml",
+		"--stdin-filepath",
+		'"%=p"',
+		"--",
+		"-",
+	},
 }
 
-vim.g.neoformat_enabled_sh             = { "shfmt" }
+vim.g.neoformat_enabled_sh = { "shfmt" }
 
-vim.g.neoformat_enabled_lua             = { "stylua" }
+vim.g.neoformat_enabled_lua = { "stylua" }
 
-vim.g.neoformat_enabled_js              = { "prettier" }
+vim.g.neoformat_enabled_js = { "prettier" }
 
 vim.g.neoformat_enabled_json            = vim.g.neoformat_enabled_js
 vim.g.neoformat_enabled_ts              = vim.g.neoformat_enabled_js
@@ -46,9 +46,9 @@ vim.g.neoformat_enabled_javascript      = vim.g.neoformat_enabled_js
 vim.g.neoformat_enabled_typescriptreact = vim.g.neoformat_enabled_js
 vim.g.neoformat_enabled_javascriptreact = vim.g.neoformat_enabled_js
 
-vim.g.neoformat_enabled_python          = { "black" }
-vim.g.neoformat_cmake_cmakeformat       = { args = {}, stdin = 1, exe = "cmake-format" }
-vim.g.neoformat_only_msg_on_error       = 0
+vim.g.neoformat_enabled_python = { "black" }
+vim.g.neoformat_cmake_cmakeformat = { args = {}, stdin = 1, exe = "cmake-format" }
+vim.g.neoformat_only_msg_on_error = 0
 
 vim.api.nvim_set_keymap(Keys.N, "<Space>cf", ":Neoformat<CR>", Keys.Noremap)
 vim.api.nvim_set_keymap(Keys.V, "<Space>cf", ":Neoformat<CR>", Keys.Noremap)

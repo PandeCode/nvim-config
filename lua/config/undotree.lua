e36c1b6b468e76ef2331fe47e15fba12039abc15
PandeCode <pandeshawnbenjamin@gmail.com>
2023-01-28 17:21:04 +0300

    mv

--- lua/config/undotree.lua
+++ lua/config/undotree.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap(Keys.NoneStr, "<C-u>", "<CMD>UndotreeToggle<CR>", Keys.Noremap)
 vim.api.nvim_set_keymap(Keys.NoneStr, "<leader>u", "<CMD>UndotreeToggle<CR>", Keys.Noremap)
 
-vim.g:undotree_WindowLayout = 4
+vim.g.undotree_WindowLayout = 4

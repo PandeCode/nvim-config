2e6fe7dd1e85068cb888e243fffb5348c25afe2c
PandeCode <pandeshawnbenjamin@gmail.com>
2023-01-29 20:22:50 +0300

    c support for enum funcs

--- lua/etc/cpp_enum_parse.lua
+++ lua/etc/cpp_enum_parse.lua
@@ -8,10 +8,12 @@ end
 local function gen_enum_funcs(enum_name, bufnr)
 	enum_name = enum_name or vim.fn.expand("<cword>")
 	bufnr = bufnr or vim.api.nvim_get_current_buf()
-	if vim.bo[bufnr].filetype ~= "cpp" then
-		notify("Can only be used in cpp", "warn", { title = IDE.name })
+	local ft = vim.bo[bufnr].filetype
+	if ft ~= "cpp" and ft ~= "c" then
+		notify("Can only be used in c or cpp", "warn", { title = IDE.name })
 		return
 	end
+	local cpp = ft == "cpp"
 
 	local root = get_root(bufnr)
 	local enum_data = vim.treesitter.parse_query("cpp", [[
@@ -42,49 +44,69 @@ local function gen_enum_funcs(enum_name, bufnr)
 		return
 	end
 
+	local err = (function()
+		if cpp then
+			return 'throw std::invalid_argument("Must be valid type '
+				.. enum_name
+				.. "::{"
+				.. table.concat(items, ", ")
+				.. '}");'
+		else
+			return 'fprintf(stderr, "Must be valid type Options::{Option1, Option2, Option3, Option4, Option5, Option6, Option7, Option8, Option9, Option10, Option11}");abort();'
+		end
+	end)()
+
 	local lines = {
 		"// clang-format off",
-		enum_name .. " " .. enum_name .. "_fromstring(const std::string& str) {",
+		cpp and enum_name .. " " .. enum_name .. "_fromstring(const std::string& str) {"
+			or enum_name .. " " .. enum_name .. "_fromstring(const char* str) {",
 	}
 
 	for _, value in pairs(items) do
-		table.insert(lines, '	if(str == "' .. value .. '") return ' .. enum_name .. "::" .. value .. ";")
+		table.insert(
+			lines,
+			cpp and '	if(str == "' .. value .. '") return ' .. enum_name .. "::" .. value .. ";"
+				or '	if(strcmp(str, "' .. value .. '") == 0) return ' .. enum_name .. "::" .. value .. ";"
+		)
 	end
 
+	table.insert(lines, "\t" .. err)
+	table.insert(lines, "}")
 	table.insert(
 		lines,
-		'	throw std::invalid_argument("Must be valid type ' .. enum_name .. "::{" .. table.concat(items, ", ") .. '}");'
+		cpp and "std::string " .. enum_name .. "_tostring(const " .. enum_name .. "& e) {"
+			or "const char* " .. enum_name .. "_tostring(const " .. enum_name .. "& e) {"
 	)
-	table.insert(lines, "}")
-	table.insert(lines, "std::string " .. enum_name .. "_tostring(const " .. enum_name .. "& e) {")
 	table.insert(lines, "	switch(e) {")
 
-	for _, value in pairs(items) do
-		table.insert(lines, "		case " .. enum_name .. "::" .. value .. ': return "' .. value .. '";')
+	if cpp then
+		for _, value in pairs(items) do
+			table.insert(lines, "		case " .. enum_name .. "::" .. value .. ': return "' .. value .. '";')
+		end
+	else
+		for _, value in pairs(items) do
+			table.insert(lines, "		case " .. enum_name .. "::" .. value .. ': return "' .. value .. '";')
+		end
 	end
 
-	table.insert(
-		lines,
-		'		default: throw std::invalid_argument("Must be valid type '
-			.. enum_name
-			.. "::{"
-			.. table.concat(items, ", ")
-			.. '}");'
-	)
+	table.insert(lines, "		default: " .. err)
 	table.insert(lines, "	}")
 	table.insert(lines, "}")
-	table.insert(lines, "std::ostream& operator<<(std::ostream& stream, const " .. enum_name .. "& e) {")
-	table.insert(lines, "	stream << " .. enum_name .. "_tostring(e);")
-	table.insert(lines, "	return stream;")
-	table.insert(lines, "}")
+
+	if cpp then
+		table.insert(lines, "std::ostream& operator<<(std::ostream& stream, const " .. enum_name .. "& e) {")
+		table.insert(lines, "	stream << " .. enum_name .. "_tostring(e);")
+		table.insert(lines, "	return stream;")
+		table.insert(lines, "}")
+	end
 	table.insert(lines, "// clang-format on")
 
 	vim.api.nvim_buf_set_lines(bufnr, start_from, start_from, false, lines)
-	notify("Added enum functions to '" .. enum_name .. "'", "ERROR", { title = IDE.name })
+	notify("Added enum functions to '" .. enum_name .. "'", "info", { title = IDE.name })
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = { "*.cpp" },
+	pattern = { "*.cpp", "*.c" },
 	group = vim.api.nvim_create_augroup("gen_enum", { clear = true }),
 	callback = function(tbl)
 		vim.api.nvim_create_user_command("GenEnum", function()

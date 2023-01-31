3679d79d5cdd24a94c4a7010651f7f8718d5828a
PandeCode <pandeshawnbenjamin@gmail.com>
2023-01-31 18:22:17 +0300

    fmt

--- lua/plugins/neodev_conf.lua
+++ lua/plugins/neodev_conf.lua
@@ -1,11 +1,13 @@
-vim.api.nvim_create_autocmd("BufRead", {
-	pattern = { "*/nvim/**/*.lua" },
-	callback = function()
-		require("neodev").setup({
-			library = {
-					plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-			}
-		})
-	end,
-})
+vim.api.nvim_create_autocmd(
+	"BufRead", {
+		pattern = {"*/nvim/**/*.lua"},
+		callback = function()
+			require("neodev").setup(
+				{
+					library = {plugins = {"nvim-treesitter", "plenary.nvim", "telescope.nvim"}}
+				}
+			)
+		end
+	}
+)
 
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,14 +1,10 @@
-IDE = { name = "Nvim" }
+IDE = {name = "Nvim"}
 
-function RandFrom(list)
-	return list[math.random(1, #list)]
-end
+function RandFrom(list) return list[math.random(1, #list)] end
 
 function RandStr(length)
 	local res = ""
-	for i = 1, length do
-		res = res .. string.char(math.random(97, 122))
-	end
+	for i = 1, length do res = res .. string.char(math.random(97, 122)) end
 	return res
 end
 
@@ -30,9 +26,7 @@ function Dump(o)
 	if type(o) == "table" then
 		local s = "{ "
 		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
+			if type(k) ~= "number" then k = "\"" .. k .. "\"" end
 			s = s .. "[" .. k .. "] = " .. Dump(v) .. ","
 		end
 		return s .. "} "
@@ -42,18 +36,14 @@ function Dump(o)
 end
 
 function PrintError(text)
-	vim.cmd.echohl({ args = { "ErrorMsg" } })
-	vim.cmd.echomsg({ args = { "'" .. text .. "'" } })
-	vim.cmd.echohl({ args = { "None" } })
+	vim.cmd.echohl({args = {"ErrorMsg"}})
+	vim.cmd.echomsg({args = {"'" .. text .. "'"}})
+	vim.cmd.echohl({args = {"None"}})
 end
 
 function Prequire(path)
-	if 1 then
-		return require(path)
-	end
-	local status, lib = xpcall(require, function(...)
-		print(...)
-	end, path)
+	if 1 then return require(path) end
+	local status, lib = xpcall(require, function(...) print(...) end, path)
 	if status then
 		return lib
 	else
@@ -64,33 +54,21 @@ function Prequire(path)
 end
 
 function RequireForFileType(ft, module)
-	vim.api.nvim_create_autocmd({ "Filetype" }, {
-		pattern = ft,
-		callback = function()
-			Prequire(module)
-		end,
-	})
+	vim.api.nvim_create_autocmd(
+		{"Filetype"}, {pattern = ft, callback = function() Prequire(module) end}
+	)
 end
 
 function RequireForPattern(pattern, module)
-	vim.api.nvim_create_autocmd({ "BufEnter" }, {
-		pattern = pattern,
-		callback = function()
-			Prequire(module)
-		end,
-	})
+	vim.api.nvim_create_autocmd(
+		{"BufEnter"}, {pattern = pattern, callback = function() Prequire(module) end}
+	)
 end
 
-function RequireFn(file)
-	return function()
-		Prequire(file)
-	end
-end
+function RequireFn(file) return function() Prequire(file) end end
 
 function RequireSetupFn(file, tbl)
-	return function()
-		Prequire(file).setup(tbl or {})
-	end
+	return function() Prequire(file).setup(tbl or {}) end
 end
 
 Keys = {
@@ -103,19 +81,19 @@ Keys = {
 	NoneStr = "",
 
 	None = {},
-	Expr = { expr = true },
-	Silent = { silent = true },
-	SilentExpr = { expr = true, silent = true },
-	Noremap = { noremap = true },
-	NoremapExpr = { noremap = true, expr = true },
-	NoremapSilent = { noremap = true, silent = true },
-	NoremapSilentExpr = { noremap = true, expr = true, silent = true },
+	Expr = {expr = true},
+	Silent = {silent = true},
+	SilentExpr = {expr = true, silent = true},
+	Noremap = {noremap = true},
+	NoremapExpr = {noremap = true, expr = true},
+	NoremapSilent = {noremap = true, silent = true},
+	NoremapSilentExpr = {noremap = true, expr = true, silent = true},
 	NoremapSilentExprScript = {
 		noremap = true,
 		silent = true,
 		script = true,
-		expr = true,
-	},
+		expr = true
+	}
 }
 ---A helper function to print a table's contents.
 ---@param tbl table @The table to print.
@@ -130,13 +108,11 @@ function PrintTable(tbl, depth, n)
 		return
 	end
 
-	if n == 0 then
-		print(" ")
-	end
+	if n == 0 then print(" ") end
 
 	for key, value in pairs(tbl) do
 		if key and type(key) == "number" or type(key) == "string" then
-			key = string.format('["%s"]', key)
+			key = string.format("[\"%s\"]", key)
 
 			if type(value) == "table" then
 				if next(value) then
@@ -148,7 +124,7 @@ function PrintTable(tbl, depth, n)
 				end
 			else
 				if type(value) == "string" then
-					value = string.format('"%s"', value)
+					value = string.format("\"%s\"", value)
 				else
 					value = tostring(value)
 				end
@@ -158,7 +134,5 @@ function PrintTable(tbl, depth, n)
 		end
 	end
 
-	if n == 0 then
-		print(" ")
-	end
+	if n == 0 then print(" ") end
 end

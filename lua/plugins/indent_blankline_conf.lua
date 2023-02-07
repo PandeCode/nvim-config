efc6de1fbed2a90b86376bc349d99179867b2e7c
PandeCode <pandeshawnbenjamin@gmail.com>
2023-02-07 21:02:17 +0300

    undotree

--- lua/plugins/indent_blankline_conf.lua
+++ lua/plugins/indent_blankline_conf.lua
@@ -1,3 +1,7 @@
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "tab: ->"
+
 require("indent_blankline").setup({
 	show_current_context = true,
 	show_current_context_start = true,

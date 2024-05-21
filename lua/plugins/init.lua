7b5aa12d610bb93ff922b61f6a788c3246041d96
PandeCode <pandeshawnbenjamin@gmail.com>
2024-05-21 22:12:41 +0000

    ssr

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -215,7 +215,32 @@ local vscode_enabled_plugins = {
 
 
 local vscode_disabled_plugins = {
-		{
+	{
+		"cshuaimin/ssr.nvim",
+		module = "ssr",
+		-- Calling setup is optional.
+		config = function()
+			require("ssr").setup {
+				border = "rounded",
+				min_width = 50,
+				min_height = 5,
+				max_width = 120,
+				max_height = 25,
+				adjust_window = true,
+				keymaps = {
+					close = "q",
+					next_match = "n",
+					prev_match = "N",
+					replace_confirm = "<cr>",
+					replace_all = "<leader><cr>",
+				},
+			}
+
+			vim.keymap.set({ "n", "x" }, "<leader>sr", function() require("ssr").open() end)
+		end
+	},
+
+	{
 		"folke/zen-mode.nvim",
 		keys = { { "<LEADER>zm", ":ZenMode<cr>", desc = "ZenMode" } },
 		priority = 0,

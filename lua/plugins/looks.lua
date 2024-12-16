59393a3160e1e72d08b005fbe4d2c0a50de4d94e
PandeCode <pandeshawnbenjamin@gmail.com>
2024-12-16 07:48:12 +0000

    buffer bindings

--- lua/plugins/looks.lua
+++ lua/plugins/looks.lua
@@ -14,6 +14,19 @@ local M = {
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
+
+		keys = {
+			{ "<LEADER>1", "<CMD>BufferLineGoToBuffer 1<CR>" },
+			{ "<LEADER>2", "<CMD>BufferLineGoToBuffer 2<CR>" },
+			{ "<LEADER>3", "<CMD>BufferLineGoToBuffer 3<CR>" },
+			{ "<LEADER>4", "<CMD>BufferLineGoToBuffer 4<CR>" },
+			{ "<LEADER>5", "<CMD>BufferLineGoToBuffer 5<CR>" },
+			{ "<LEADER>6", "<CMD>BufferLineGoToBuffer 6<CR>" },
+			{ "<LEADER>7", "<CMD>BufferLineGoToBuffer 7<CR>" },
+			{ "<LEADER>8", "<CMD>BufferLineGoToBuffer 8<CR>" },
+			{ "<LEADER>9", "<CMD>BufferLineGoToBuffer 9<CR>" },
+		},
+
 		opts = {
 			options = {
 				diagnostics = "nvim_lsp",

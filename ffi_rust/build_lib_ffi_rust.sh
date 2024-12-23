e17e64c44665d45959f0468ac984c46095b8ca78
PandeCode <pandeshawnbenjamin@gmail.com>
2024-12-23 23:43:00 +0000

    style: ffi_rust organisation

--- ffi_rust/build_lib_ffi_rust.sh
+++ ffi_rust/build_lib_ffi_rust.sh
@@ -2,8 +2,6 @@
 
 mkdir -p ~/.cache/target/release
 
-cd ffi_rust
 nix-shell --run "cargo build --release"
-cd ..
 
-cp ~/.cache/target/release/libffi_rust.so lua
+cp ~/.cache/target/release/libffi_rust.so ../lua
